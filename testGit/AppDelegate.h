5e1a5d538485bf15dcd883a61a99f8388b578850
xueqiaoqiang <qiaoqiang.xue@heysroad.com>
2016-05-05 09:04:06 +0800

    添加版本控制

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by xueqiaoqiang on 16/5/5.
+//  Copyright © 2016年 xqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by xueqiaoqiang on 16/5/5.
+//  Copyright © 2016年 xqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
